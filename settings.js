d700387e1d7c1cb6deefa1dc7339593d46b048e1
xzc project <99465722+xzc-mxn@users.noreply.github.com>
2026-07-01 09:49:24 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -95,7 +95,7 @@ window.Settings = (() => {
         gemini: [
             { id: 'gemini-3.5-flash', name: 'Gemini 3.5 Flash' },
             { id: 'gemini-2.5-flash', name: 'Gemini 2.5 Flash' },
-            { id: 'gemini-3.5-pro', name: 'Gemini 3.5 Pro'}
+            { id: 'gemini-2.5-pro', name: 'Gemini 2.5 Pro'}
         ]
     };
 
